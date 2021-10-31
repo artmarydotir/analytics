a26325f5e1498b41c453672a66d7090156a034a3
Art Mary <artmary.ir@gmail.com>
2021-10-31 19:20:13 +0330

    feat: user profile

---
+++ app/api/src/GraphQL/Query/User/UserProfile.js
@@ -0,0 +1,37 @@
+const { ErrorWithProps } = require('mercurius').default;
+const { constants: userRoles } = require('../../../Schema/UserRoles');
+
+const {
+  constantsMerge: errorConstMerge,
+} = require('../../../Schema/ErrorMessage');
+
+module.exports = async (_, { data }, { container, token }) => {
+  const { UserProfileRepository } = container;
+  const { id } = data;
+
+  if (!token) {
+    throw new ErrorWithProps(errorConstMerge.FORBIDDEN, {
+      statusCode: 403,
+    });
+  }
+
+  if (
+    !token.roles ||
+    id !== token.uid ||
+    token.roles !== userRoles.SUPERADMIN
+  ) {
+    throw new ErrorWithProps(errorConstMerge.FORBIDDEN, {
+      statusCode: 403,
+    });
+  }
+
+  const user = UserProfileRepository.returnUserData(id);
+
+  if (!user) {
+    throw new ErrorWithProps(errorConstMerge.NOT_EXIST, {
+      statusCode: 404,
+    });
+  }
+
+  return user;
+};
---
+++ app/front-cp/components/User/UserProfile.vue
@@ -0,0 +1,117 @@
+<template>
+  <v-row justify="center" class="mt-12 pt-4">
+    <v-col
+      cols="12"
+      md="9"
+      lg="8"
+      :class="$vuetify.theme.dark ? 'elevation-10' : 'elevation-7'"
+    >
+      <v-row v-if="profile">
+        <v-col cols="12" md="5">
+          <UserAvatar />
+        </v-col>
+        <v-divider vertical></v-divider>
+        <v-col cols="12" md="7">
+          <v-card flat class="mx-auto my-12" max-width="500">
+            <v-card-title>
+              <v-icon color="primary" large left> mdi-account </v-icon>
+              Welcome {{ profile.username }}
+            </v-card-title>
+
+            <v-card-text>
+              <div class="my-8">
+                <v-row no-gutters>
+                  <v-col cols="12" md="6" class="flex-grow-0 flex-shrink-0">
+                    <v-card class="pa-2" outlined tile> Email: </v-card>
+                  </v-col>
+                  <v-col cols="12" md="6" class="flex-grow-0 flex-shrink-0">
+                    <v-card class="pa-2" outlined tile>
+                      {{ profile.email }}
+                    </v-card>
+                  </v-col>
+                  <v-col cols="12" md="6" class="flex-grow-0 flex-shrink-0">
+                    <v-card class="pa-2" outlined tile> Mobile: </v-card>
+                  </v-col>
+                  <v-col
+                    v-if="profile.mobile"
+                    cols="12"
+                    md="6"
+                    class="flex-grow-0 flex-shrink-0"
+                  >
+                    <v-card class="pa-2" outlined tile>
+                      {{ profile.mobile }}
+                    </v-card>
+                  </v-col>
+                  <v-col cols="12" md="6" class="flex-grow-0 flex-shrink-0">
+                    <v-card class="pa-2" outlined tile> Status </v-card>
+                  </v-col>
+
+                  <v-col cols="12" md="6" class="flex-grow-0 flex-shrink-0">
+                    <v-card v-if="profile.options" class="pa-2" outlined tile>
+                      <v-chip x-small color="green" dark>
+                        {{ calcStatus }}
+                      </v-chip>
+                    </v-card>
+                  </v-col>
+                </v-row>
+              </div>
+            </v-card-text>
+
+            <v-divider class="mx-4"></v-divider>
+
+            <v-card-text class="d-flex align-center">
+              <v-card-title class="pr-0 pl-0">Role:</v-card-title>
+              <v-chip color="primary darken-2 accent-4 ml-3 mr-3" dark small>
+                <span v-if="profile.role === 'AD'"> Admin </span>
+                <span v-if="profile.role === 'VI'"> Viewer </span>
+                <span v-if="profile.role === 'SA'"> Super Admin </span>
+              </v-chip>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="deep-purple lighten-2"
+                text
+                @click="edit(profile.id)"
+              >
+                Edit profile
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    profile: {
+      type: Object,
+      default: () => {},
+      required: true,
+    },
+  },
+  computed: {
+    calcStatus() {
+      const optionList = this.profile.options;
+      if (optionList.includes(1)) {
+        return 'Active';
+      }
+      return 'Unknown';
+    },
+  },
+  methods: {
+    edit(uid) {
+      this.$router.push(
+        this.localeRoute({
+          name: 'user-edit-id',
+          params: { id: uid },
+        }),
+      );
+    },
+  },
+};
+</script>
