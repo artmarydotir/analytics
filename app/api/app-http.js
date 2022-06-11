dcae4b7eaa4b332d6cef70b62b15e1a6833635d2
Art Mary <artmary.ir@gmail.com>
2022-06-11 22:23:34 +0430

    fix: fix ci errors

--- app/api/app-http.js
+++ app/api/app-http.js
@@ -21,36 +21,7 @@ const init = require('./init');
 
     await app.listen(Config.ASM_APP_PORT, '0.0.0.0');
   } catch (e) {
+    console.log(e);
     process.exit(1);
   }
 })();
-
-// const { Config: ConfigClass } = require('./src/Config');
-// const { initContainer } = require('./src/Container');
-// const { ConfigSchema } = require('./src/ConfigSchema');
-// const { Logger } = require('./src/Logger');
-
-// (async () => {
-//   // create config
-//   const ConfigInstance = new ConfigClass(ConfigSchema, process.env);
-//   const Config = ConfigInstance.getAll();
-
-//   // list of loggers
-//   const Loggers = [];
-//   ['FastifyLogger', 'MongooseLogger', 'PostgresLogger'].forEach((name) => {
-//     const logger = new Logger(
-//       Config.ASM_PUBLIC_APP_NS,
-//       name,
-//       Config.ASM_LOG_LEVEL,
-//       Config.ASM_PUBLIC_APP_TEST,
-//     );
-//     Loggers.push({
-//       name,
-//       value: logger,
-//     });
-//   });
-
-//   const container = await initContainer(ConfigInstance, Loggers);
-
-//   await container.dispose();
-// })();
